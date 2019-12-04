881a9f8c0c231248d579298ed2c23ea52867e5aa
Gus <g.sosacuervo1@gmail.com>
2019-12-03 20:33:13 -0600

    Solution to problem Reverse Integer
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/

---
+++ InterviewQuestions/Strings/ReverseInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReverseInteger {
+  class Program {
+    public int Reverse(int x) {
+      int mul = x < 0 ? -1 : 1;
+      int reverse = 0;
+      x *= mul;
+      while (x > 0) {
+        try {
+          checked {
+            reverse = reverse * 10 + x % 10;
+          }
+        } catch (Exception) {
+          return 0;
+        }
+        x /= 10;
+      }
+      return reverse * mul;
+    }
+
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.Reverse(123) == 321);
+      Console.WriteLine(p.Reverse(120) == 21);
+      Console.WriteLine(p.Reverse(-123) == -321);
+      Console.WriteLine(p.Reverse(1534236469) == 0);
+    }
+  }
+}
