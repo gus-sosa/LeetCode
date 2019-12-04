87530306d7b05d0f10582c99130fb5d1b968b9d2
Gus <g.sosacuervo1@gmail.com>
2019-12-03 21:05:45 -0600

    Solution to problem Valid Anagram
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/

---
+++ InterviewQuestions/Strings/ValidAnagram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ValidAnagram {
+  class Program {
+    public bool IsAnagram(string s, string t) {
+      int alphabetLength = 256;
+      int[] map = new int[alphabetLength];
+      foreach (var letter in s) {
+        ++map[letter];
+      }
+      foreach (var letter in t) {
+        --map[letter];
+      }
+      return map.All(i => i == 0);
+    }
+
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.IsAnagram("anagram", "nagaram") == true);
+      Console.WriteLine(p.IsAnagram("rat", "car") == false);
+    }
+  }
+}
