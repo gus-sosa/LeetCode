9f380c72036a887f482f570e4c3012c94773d528
Gus <g.sosacuervo1@gmail.com>
2019-12-03 19:54:38 -0600

    Solution to problem Reverse String
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/

---
+++ InterviewQuestions/Strings/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseString {
+  class Program {
+    public void ReverseString(char[] s) {
+      for (int i = 0, j = s.Length - 1; i < j; ++i, --j) {
+        char tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+      }
+    }
+
+    static void Main(string[] args) {
+      var p = new Program();
+      char[] input = new char[] { 'a', 'b', 'c' };
+      p.ReverseString(input);
+      CompareArray(input, new char[] { 'c', 'b', 'a' });
+    }
+
+    private static void CompareArray(char[] v, char[] result) {
+      Console.WriteLine(Enumerable.Zip(v, result, (i1, i2) => i1 == i2).All(i => i));
+    }
+  }
+}
