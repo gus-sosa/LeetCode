8076203b8a1a39e2e3c9e756c2a82e829b0004a2
Gus <gcuervo@gtreasury.com>
2019-12-04 17:05:20 -0600

    Creating solution for problem StrStr

---
+++ InterviewQuestions/Strings/StrStr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StrStr
+{
+  class Program
+  {
+    public int StrStr(string haystack, string needle) {
+
+    }
+
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.StrStr("gus", "") == 0);
+      Console.WriteLine(p.StrStr("gus", "g") == 0);
+      Console.WriteLine(p.StrStr("gus", "gu") == 0);
+      Console.WriteLine(p.StrStr("gus", "us") == 1);
+      Console.WriteLine(p.StrStr("gus", "s") == 2);
+    }
+  }
+}
