e23ed56636b6e85de0c590da8043e33afb1cc436
Gus <g.sosacuervo1@gmail.com>
2019-12-04 20:27:44 -0600

    First solution
    time limit

---
+++ InterviewQuestions/Strings/LongestCommonPrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LongestCommonPrefix {
+  class Program {
+    public string LongestCommonPrefix(string[] strs) {
+      int index = 0;
+      bool flag = true;
+      while (flag) {
+        foreach (string s in strs) {
+          if (index >= s.Length || s[index] != strs[0][index]) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          ++index;
+        }
+      }
+      return strs[0].Substring(0, index);
+    }
+
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.LongestCommonPrefix(new[] { "flower", "flow", "flight" }) == "fl");
+      Console.WriteLine(p.LongestCommonPrefix(new[] { "dog", "racecar", "car" }) == "");
+    }
+  }
+}
