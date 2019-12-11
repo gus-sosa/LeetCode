d3f01ad4bae32b32ed6f4e96fdeb523fb6c7b1b1
Gus <gcuervo@gtreasury.com>
2019-12-11 12:48:55 -0600

    First solution iteratively
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/

---
+++ InterviewQuestions/LinkedList/RerverseLinkedList/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+    }
+
+
+    public ListNode ReverseList(ListNode head) {
+      ListNode result = null;
+      while (head != null) {
+        var node = new ListNode(head.val);
+        node.next = result;
+        result = node;
+        head = head.next;
+      }
+      return result;
+    }
+
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
