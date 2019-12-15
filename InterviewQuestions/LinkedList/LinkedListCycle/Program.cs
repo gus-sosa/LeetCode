e8b66893244e972d0b81144923dec74b4580605c
Gus <g.sosacuervo1@gmail.com>
2019-12-15 01:54:10 -0600

    Solution to problem Linked List Cycle
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/773/

---
+++ InterviewQuestions/LinkedList/LinkedListCycle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinkedListCycle {
+
+  public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+      val = x;
+      next = null;
+    }
+  }
+
+
+  class Program {
+    public bool HasCycle(ListNode head) {
+      if (head == null) {
+        return false;
+      }
+      ListNode tmp, current = head, visitedMakerNode = new ListNode(0);
+      while (current.next != null) {
+        if (current.next == visitedMakerNode) {
+          return true;
+        }
+        tmp = current.next;
+        current.next = visitedMakerNode;
+        current = tmp;
+      }
+      return false;
+    }
+
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
