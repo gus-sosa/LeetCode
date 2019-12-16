c9a6fd043f25a3dc20aadcf7f604d725e70979ca
Gus <g.sosacuervo1@gmail.com>
2019-12-16 03:43:16 -0600

    Solution to problem Merge Sorted Array
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/96/sorting-and-searching/587/

---
+++ InterviewQuestions/SortingAndSearching/MergeSortedArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MergeSortedArray {
+  class Program {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+      int[] result = new int[n + m];
+      int index = 0, i, j;
+      for (i = 0, j = 0; i < m && j < n;) {
+        if (nums1[i] < nums2[j]) {
+          result[index] = nums1[i];
+          ++i;
+        } else {
+          result[index] = nums2[j];
+          ++j;
+        }
+        ++index;
+      }
+      while (i < m) {
+        result[index] = nums1[i];
+        ++index; ++i;
+      }
+      while (j < n) {
+        result[index] = nums2[j];
+        ++index; ++j;
+      }
+      for (int k = 0; k < result.Length; k++) {
+        nums1[k] = result[k];
+      }
+    }
+
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
