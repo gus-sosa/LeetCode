bbaf8548cf4354897bf5afcafbdb62a6519f0c1c
Gus <g.sosacuervo1@gmail.com>
2020-01-20 01:16:32 -0600

    Scaffolding project to resolve problem House Robber
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/97/dynamic-programming/576/

---
+++ InterviewQuestions/DynamicProgramming/HouseRobber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HouseRobber {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.Rob(new int[] { 1, 2, 3, 1 }) == 4);
+      Console.WriteLine(p.Rob(new int[] { 2, 7, 9, 3, 1 }) == 12);
+    }
+
+    public int Rob(int[] nums) {
+      throw new NotImplementedException();
+    }
+  }
+}
