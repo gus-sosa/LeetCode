8af450f4c1d70cca813e9d5f512b9cf21e551a13
Gus <g.sosacuervo1@gmail.com>
2020-01-20 01:27:14 -0600

    Accepted solution

--- InterviewQuestions/DynamicProgramming/HouseRobber/Program.cs
+++ InterviewQuestions/DynamicProgramming/HouseRobber/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HouseRobber {
   class Program {
@@ -6,10 +7,24 @@ namespace HouseRobber {
       var p = new Program();
       Console.WriteLine(p.Rob(new int[] { 1, 2, 3, 1 }) == 4);
       Console.WriteLine(p.Rob(new int[] { 2, 7, 9, 3, 1 }) == 12);
+      Console.WriteLine(p.Rob(new int[0]) == 0);
     }
 
     public int Rob(int[] nums) {
-      throw new NotImplementedException();
+      if (nums.Length == 0) {
+        return 0;
+      }
+      var maxs = new int[nums.Length];
+      maxs[0] = nums[0];
+      if (nums.Length > 1) {
+        maxs[1] = Math.Max(nums[0], nums[1]);
+      }
+      if (nums.Length > 2) {
+        for (int i = 2; i < nums.Length; ++i) {
+          maxs[i] = Math.Max(maxs[i - 1], nums[i] + maxs[i - 2]);
+        }
+      }
+      return maxs.Max();
     }
   }
 }
