0f648e3e269a9c857eb72ded011f2a65988333d3
Gus <g.sosacuervo1@gmail.com>
2020-01-21 02:29:43 -0600

    Solution to problem Valid Parentheses
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/721/

---
+++ InterviewQuestions/Others/ValidParentheses/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ValidParentheses {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.IsValid("(]") == false);
+    }
+
+    public bool IsValid(string s) {
+      var stack = new Stack<char>();
+      char top;
+      foreach (char c in s) {
+        if (c == '(' || c == '{' || c == '[') {
+          stack.Push(c);
+        } else {
+          if (stack.Count > 0) {
+            top = stack.Pop();
+            if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+              return false;
+            }
+          } else {
+            return false;
+          }
+        }
+      }
+      return stack.Count == 0;
+    }
+  }
+}
