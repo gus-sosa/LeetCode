f9de04687311900ad6ff6949715abb2020463233
Gus <g.sosacuervo1@gmail.com>
2020-01-21 02:37:33 -0600

    Solution to problem Missing Number
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/722/

---
+++ InterviewQuestions/Others/MissingNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace MissingNumber {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public int MissingNumber(int[] nums) {
+      int sum = nums.Length * (nums.Length + 1) / 2;
+      return sum - nums.Sum();
+    }
+  }
+}
