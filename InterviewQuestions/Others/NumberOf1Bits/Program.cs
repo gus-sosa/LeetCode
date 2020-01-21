5549ef763e0848df0484c71c098538142313ae42
Gus <g.sosacuervo1@gmail.com>
2020-01-21 01:28:08 -0600

    Solution to problem Number of 1 bits
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/565/

---
+++ InterviewQuestions/Others/NumberOf1Bits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberOf1Bits {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public int HammingWeight(uint n) {
+      int result = 0;
+      for (int i = 0; i < 32; ++i) {
+        if ((n & 1) == 1) {
+          ++result;
+        }
+        n >>= 1;
+      }
+      return result;
+    }
+  }
+}
