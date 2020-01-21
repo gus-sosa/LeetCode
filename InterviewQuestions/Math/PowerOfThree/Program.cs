603ea4d356694aad622a71977eff663be9fe8a61
Gus <g.sosacuervo1@gmail.com>
2020-01-20 20:36:16 -0600

    Solutions to problem power of three
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/745/

---
+++ InterviewQuestions/Math/PowerOfThree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerOfThree {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.IsPowerOfThree(27) == true);
+      Console.WriteLine(p.IsPowerOfThree(0) == false);
+      Console.WriteLine(p.IsPowerOfThree(9) == true);
+      Console.WriteLine(p.IsPowerOfThree(45) == false);
+    }
+
+    //public bool IsPowerOfThree(int n) {
+    //  while (n > 1 && n % 3 == 0) {
+    //    n /= 3;
+    //  }
+    //  return n == 1;
+    //}
+
+    public bool IsPowerOfThree(int n) {
+      return n > 0 && 1162261467 % n == 0;
+    }
+
+    //public bool IsPowerOfThree(int n) {
+    //  int tmp;
+    //  return n == 1 || (n % 3 == 0 && (n % 10 == 1 || n % 10 == 3 || n % 10 == 9 || n % 10 == 7) && int.TryParse(Math.Log(n, 3).ToString(), out tmp));
+    //}
+  }
+}
