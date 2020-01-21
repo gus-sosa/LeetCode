376ca776729789d195ad50d62b5d6d4f3dd2926f
Gus <g.sosacuervo1@gmail.com>
2020-01-21 01:40:16 -0600

    Solution to problem reverse bits
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/648/

---
+++ InterviewQuestions/Others/ReverseBits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseBits {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public uint reverseBits(uint n) {
+      uint result = 0;
+      for (int i = 0; i < 32; ++i, n >>= 1) {
+        result <<= 1;
+        result ^= (n & 1);
+      }
+      return result;
+    }
+  }
+}
