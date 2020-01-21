dd52fedc78cd2b7eb3dfeff1af32a8bdca00fa42
Gus <g.sosacuervo1@gmail.com>
2020-01-21 01:34:09 -0600

    Solution to problem hamming distance
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/99/others/762/

---
+++ InterviewQuestions/Others/HammingDistance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HammingDistance {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.HammingDistance(1, 4) == 2);
+    }
+
+    public int HammingDistance(int x, int y) {
+      int result = 0;
+      for (int i = 0; i < 32; ++i, x >>= 1, y >>= 1) {
+        if ((x & 1) != (y & 1)) {
+          ++result;
+        }
+      }
+      return result;
+    }
+  }
+}
