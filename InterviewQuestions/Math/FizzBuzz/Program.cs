3c0f5c4ec8f4a0eee938e967b2f139a489276e8c
Gus <g.sosacuervo1@gmail.com>
2020-01-20 19:51:03 -0600

    Solution to problem Fizz Buzz
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/

---
+++ InterviewQuestions/Math/FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public IList<string> FizzBuzz(int n) {
+      var list = new List<string>();
+      bool mul3 = false, mul5 = false;
+      string fizz = "Fizz", buzz = "Buzz", result = string.Empty;
+      for (int i = 1; i <= n; ++i, result = string.Empty) {
+        if (mul3 = (i % 3 == 0)) {
+          result += fizz;
+        }
+        if (mul5 = (i % 5 == 0)) {
+          result += buzz;
+        }
+        if (!mul3 && !mul5) {
+          result = i.ToString();
+        }
+        list.Add(new string(result.ToCharArray()));
+      }
+      return list;
+    }
+  }
+}
