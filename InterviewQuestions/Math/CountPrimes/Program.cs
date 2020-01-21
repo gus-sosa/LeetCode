91df9754a50d0033e0fbacf4809190d4f586a172
Gus <g.sosacuervo1@gmail.com>
2020-01-20 20:06:05 -0600

    Solution to problem Count Primes
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/744/

---
+++ InterviewQuestions/Math/CountPrimes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace CountPrimes {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.CountPrimes(10) == 4);
+    }
+
+    public int CountPrimes(int n) {
+      bool[] map = new bool[n];
+      for (int i = 2; i < map.Length; ++i) {
+        map[i] = true;
+      }
+      for (int i = 2, j, nonPrimePos; i < map.Length; ++i) {
+        j = 2;
+        nonPrimePos = i * j;
+        while (nonPrimePos < n) {
+          map[nonPrimePos] = false;
+          nonPrimePos = i * ++j;
+        }
+      }
+      return map.Count(i => i);
+    }
+  }
+}
