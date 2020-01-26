d6b98492a0db0f2aec4117a2745349ed15b0decc
Gus <g.sosacuervo1@gmail.com>
2020-01-26 00:58:35 -0600

    Solution to problem Longest substring without repeating characters
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/780/

---
+++ InterviewQuestions/Array/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestSubstringWithoutRepeatingCharacters {
+  class Program {
+    static void Main(string[] args) {
+      var p = new Program();
+      Console.WriteLine(p.LengthOfLongestSubstring("pwwkew") == 3);
+    }
+
+    public int LengthOfLongestSubstring(string s) {
+      var dict = new Dictionary<char, int>();
+      int max = 0, start = 0;
+      for (int i = 0; i < s.Length; ++i) {
+        char c = s[i];
+        if (dict.ContainsKey(c)) {
+          max = Math.Max(max, dict.Count);
+          int index = dict[c];
+          int newStart = index + 1;
+          while (index >= start) {
+            dict.Remove(s[index--]);
+          }
+          start = newStart;
+        }
+        dict[c] = i;
+      }
+      return Math.Max(max, dict.Count);
+    }
+  }
+}
