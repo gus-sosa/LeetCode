061fb0ee65b750f0a03af7331bf90e06ac47792a
Gus <g.sosacuervo1@gmail.com>
2020-01-26 00:32:58 -0600

    Solution to problem Group Anagrams
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/778/

---
+++ InterviewQuestions/Array/GroupAnagrams/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroupAnagrams {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+      var dict = new Dictionary<string, List<string>>();
+      foreach (var str in strs) {
+        var strArr = str.ToCharArray();
+        Array.Sort(strArr);
+        string key = new string(strArr);
+        if (!dict.ContainsKey(key)) {
+          dict[key] = new List<string>();
+        }
+        dict[key].Add(str);
+      }
+      var result = new List<IList<string>>();
+      foreach (string key in dict.Keys) {
+        result.Add(dict[key]);
+      }
+      return result;
+    }
+  }
+}
