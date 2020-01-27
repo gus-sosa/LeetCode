f1a34a69c30a9d030dcd68209cbb7645278eebed
Gus <g.sosacuervo1@gmail.com>
2020-01-27 14:45:29 -0600

    Solution to problem Odd Even Linked List
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/107/linked-list/784/

---
+++ InterviewQuestions/LinkedList/OddEvenLinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OddEvenLinkedList {
+  class Program {
+    /**
+ * Definition for singly-linked list.*/
+    public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+    }
+
+
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    public ListNode OddEvenList(ListNode head) {
+      if (head == null) {
+        return head;
+      }
+      ListNode evenListHead = new ListNode(0), evenListTail = evenListHead, current = head, lastOddNode = head, next;
+      int index = 0;
+      while (current != null) {
+        next = current.next;
+        if (++index % 2 == 0) {
+          lastOddNode.next = current.next;
+          evenListTail.next = current;
+          evenListTail = evenListTail.next;
+          evenListTail.next = null;
+        } else {
+          lastOddNode = current;
+        }
+        current = next;
+      }
+      lastOddNode.next = evenListHead.next;
+      return head;
+    }
+  }
+}
