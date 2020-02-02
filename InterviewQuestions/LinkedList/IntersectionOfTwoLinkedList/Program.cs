255b00037b207b7b86e3ccd9a7815552b764d0ed
Gus <g.sosacuervo1@gmail.com>
2020-02-01 19:09:50 -0600

    Solution to problem Intersection Of Two Linked List
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/107/linked-list/785/

---
+++ InterviewQuestions/LinkedList/IntersectionOfTwoLinkedList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntersectionOfTwoLinkedList {
+  class Program {
+    /**
+ * Definition for singly-linked list.*/
+    public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+
+      public ListNode(IEnumerable<int> nums) {
+        int l = nums.Count();
+        if (l > 0) {
+          val = nums.First();
+          if (l - 1 > 0) {
+            next = new ListNode(nums.Skip(1));
+          }
+        }
+      }
+    }
+
+
+    static void Main(string[] args) {
+      Console.WriteLine();
+    }
+
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+      var set = new HashSet<ListNode>();
+      while (headA != null) {
+        set.Add(headA);
+        headA = headA.next;
+      }
+      while (headB != null) {
+        if (set.Contains(headB)) {
+          return headB;
+        }
+        headB = headB.next;
+      }
+      return null;
+    }
+  }
+}
