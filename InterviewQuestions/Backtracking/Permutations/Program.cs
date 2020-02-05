49c6c04fcc6a97d9408b9216d7c40da2ac873a2e
Gus <g.sosacuervo1@gmail.com>
2020-02-05 01:36:04 -0600

    Solution to problem Permutations
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/109/backtracking/795/

---
+++ InterviewQuestions/Backtracking/Permutations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutations {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+
+  #region MyRegion
+
+
+  public class Solution {
+    public IList<IList<int>> Permute(int[] nums) {
+      var result = new List<IList<int>>();
+      foreach (var item in Permute(nums, 0)) {
+        result.Add(item);
+      }
+      return result;
+    }
+
+    private IEnumerable<IList<int>> Permute(int[] nums, int currentPos) {
+      if (nums.Length == currentPos) {
+        yield return nums.ToList();
+      } else {
+        foreach (var item in Permute(nums, currentPos + 1)) {
+          yield return item;
+        }
+        for (int i = currentPos + 1, tmp = 0; i < nums.Length; ++i) {
+          tmp = nums[currentPos];
+          nums[currentPos] = nums[i];
+          nums[i] = tmp;
+          foreach (var item in Permute(nums, currentPos + 1)) {
+            yield return item;
+          }
+          tmp = nums[currentPos];
+          nums[currentPos] = nums[i];
+          nums[i] = tmp;
+        }
+      }
+    }
+  }
+
+  #endregion
+}
