79ac157287f68a27bf9818dc94cf73398dd54ddf
Gus <g.sosacuervo1@gmail.com>
2020-02-10 13:01:02 -0600

    First solution to problem Search For A Range
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/802/

---
+++ InterviewQuestions/SortingAndSearching/SearchForARange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SearchForARange {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    #region MyRegion
+
+    public class Solution {
+      public int[] SearchRange(int[] nums, int target) {
+        int[] result = new int[2] { -1, -1 };
+        for (int i = 0; i < nums.Length; ++i) {
+          if (nums[i] == target) {
+            if (result[0] == -1) {
+              result[0] = result[1] = i;
+            } else {
+              result[1] = i;
+            }
+          }
+        }
+        return result;
+      }
+    }
+
+    #endregion
+  }
+}
