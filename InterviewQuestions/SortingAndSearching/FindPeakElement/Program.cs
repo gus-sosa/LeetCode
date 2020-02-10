253548e7cf2cb6e6e7280cb1bd210a51064807db
Gus <g.sosacuervo1@gmail.com>
2020-02-10 11:42:01 -0600

    Solution to problem Find Peak Element
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/801/

---
+++ InterviewQuestions/SortingAndSearching/FindPeakElement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindPeakElement {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    #region MyRegion
+
+    public class Solution {
+      public int FindPeakElement(int[] nums) {
+        for (int i = 0; i < nums.Length - 1; ++i) {
+          if (nums[i] > nums[i + 1]) {
+            return i;
+          }
+        }
+        return nums.Length - 1;
+      }
+    }
+
+    #endregion
+  }
+}
