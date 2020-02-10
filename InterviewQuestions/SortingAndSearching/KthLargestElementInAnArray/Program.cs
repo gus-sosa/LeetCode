b6f6a0cdebefa85215103db9eb5d585def79e634
Gus <g.sosacuervo1@gmail.com>
2020-02-10 02:21:56 -0600

    Solution to problem Kth Largest Element in an Array
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/800/

---
+++ InterviewQuestions/SortingAndSearching/KthLargestElementInAnArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KthLargestElementInAnArray {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+
+    #region MyRegion
+
+    public class Solution {
+      public int FindKthLargest(int[] nums, int k) {
+        Array.Sort(nums);
+        return nums[nums.Length - k];
+      }
+    }
+
+
+    #endregion
+  }
+}
