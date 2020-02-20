480e2672eed8ce4bbc6d5588e50e32ddb354d6ff
Gus <g.sosacuervo1@gmail.com>
2020-02-20 13:53:10 -0600

    Solution to problem Pow
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/113/math/818/

---
+++ InterviewQuestions/Math/Pow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pow {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      //Console.WriteLine(s.MyPow(2.0, 10));
+      //Console.WriteLine(s.MyPow(2.1, 3));
+      //Console.WriteLine(s.MyPow(2.0, -2));
+      Console.WriteLine(s.MyPow(2.0, -2147483648));
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      public double MyPow(double x, int n) {
+        if (n == 0 || x == 1) {
+          return 1.0;
+        }
+        if (n < 0) {
+          return 1 / (n == int.MinValue ? MyPow(x, int.MaxValue - 1) : MyPow(x, -1 * n));
+        }
+        double result = MyPow(x, n / 2);
+        result *= result;
+        if (n % 2 != 0) {
+          result = x * result;
+        }
+        return result;
+      }
+    }
+
+
+    #endregion
+  }
+}
