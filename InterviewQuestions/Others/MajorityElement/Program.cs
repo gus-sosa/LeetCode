150e4a28199207dd20e6f3db2cd29a536ec4b61e
Gus <g.sosacuervo1@gmail.com>
2020-02-20 23:55:21 -0600

    Solution to problem Majority Element
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/114/others/824/

---
+++ InterviewQuestions/Others/MajorityElement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MajorityElement {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      Console.WriteLine(s.MajorityElement(new int[] { 3, 2, 3 }) == 3);
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      public int MajorityElement(int[] nums) {
+        var dict = new Dictionary<int, int>();
+        foreach (var item in nums) {
+          if (!dict.ContainsKey(item)) {
+            dict[item] = 0;
+          }
+          ++dict[item];
+        }
+        int element = 0, count = int.MinValue;
+        foreach (var item in dict) {
+          if (item.Value > count) {
+            element = item.Key;
+            count = item.Value;
+          }
+        }
+        return element;
+      }
+    }
+
+
+    #endregion
+
+
+  }
+}
