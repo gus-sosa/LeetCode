3e94d1a87282c061eeb12c8c0363449d8de4e759
Gus <g.sosacuervo1@gmail.com>
2020-02-22 01:03:19 -0600

    First solution to the problem

---
+++ InterviewQuestions/Others/TaskScheduler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskScheduler {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      Console.WriteLine(s.LeastInterval(new char[] { 'A', 'A', 'A', 'B', 'B', 'B' }, 0) == 8);
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      class ElementInfo {
+        public char Element { get; set; }
+        public int Count { get; set; }
+      }
+
+      public int LeastInterval(char[] tasks, int n) {
+        Queue<ElementInfo> taskInfos = BuildTasks(tasks);
+        int intervals = 0, i;
+        while (taskInfos.Count > 0) {
+          for (i = 0; i < n && taskInfos.Count > 0; ++i) {
+            ++intervals;
+            var current = taskInfos.Dequeue();
+            --current.Count;
+            if (current.Count > 0) {
+              taskInfos.Enqueue(current);
+            }
+          }
+          if (taskInfos.Count > 0) {
+            ++intervals;
+          }
+        }
+        return intervals;
+      }
+
+      private Queue<ElementInfo> BuildTasks(char[] tasks) {
+        var dict = new Dictionary<char, int>();
+        foreach (var item in tasks) {
+          if (!dict.ContainsKey(item)) {
+            dict[item] = 0;
+          }
+          ++dict[item];
+        }
+        var result = new Queue<ElementInfo>();
+        foreach (var item in dict) {
+          result.Enqueue(new ElementInfo() { Count = item.Value, Element = item.Key });
+        }
+        return result;
+      }
+    }
+
+
+    #endregion
+
+
+  }
+}
