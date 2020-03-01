d9167cb9d51c22624c445ccdc6b27b1aa1c468c4
Gus <g.sosacuervo1@gmail.com>
2020-03-01 00:45:55 -0600

    Solution to problem Coin Change
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/111/dynamic-programming/809/

--- InterviewQuestions/DynamicProgramming/CoinChange/Program.cs
+++ InterviewQuestions/DynamicProgramming/CoinChange/Program.cs
@@ -10,6 +10,7 @@ namespace CoinChange {
       Console.WriteLine(s.CoinChange(new int[] { 2 }, 3) == -1);
       Console.WriteLine(s.CoinChange(new int[] { 1, 2, 5 }, 5) == 1);
       Console.WriteLine(s.CoinChange(new int[] { 1, 2, 5 }, 2) == 1);
+      //Console.WriteLine(s.CoinChange(new int[] { 3, 2, 5 }, 11));
       Console.WriteLine(s.CoinChange(new int[] { 3, 2, 5 }, 11) == 3);
       Console.WriteLine(s.CoinChange(new int[] { 336, 288, 378, 16, 319, 146 }, 9212) == 26);
       Console.WriteLine(s.CoinChange(new int[] { 336, 288, 378, 16, 319, 146 }, 9212));
@@ -20,37 +21,29 @@ namespace CoinChange {
 
 
     public class Solution {
+      private int[] _coins;
+      private Dictionary<int, int> _map;
+
       public int CoinChange(int[] coins, int amount) {
-        Array.Sort(coins);
-        Array.Reverse(coins);
-        var coinAmounts = new int[coins.Length];
-        int result = -1;
-        CoinChange(coins, amount, 0, ref result);
-        return result;
+        this._coins = coins;
+        this._map = new Dictionary<int, int>();
+        _map[0] = 0;
+        return CoinChange(amount);
       }
 
-      private bool CoinChange(int[] coins, int amount, int pos, ref int count) {
-        if (amount == 0) {
-          count = 0;
-          return true;
+      private int CoinChange(int amount) {
+        if (_map.ContainsKey(amount)) {
+          return _map[amount];
         }
-        if (pos == coins.Length) { 
-          return false;
-        } else {
-          int localMin = int.MaxValue, tmpCount = -1;
-          bool flag = false;
-          for (int i = 1, l = amount / coins[pos]; i <= l; ++i) {
-            if (CoinChange(coins, amount - coins[pos] * i, pos + 1, ref tmpCount)) {
-              flag = true;
-              localMin = Math.Min(localMin, tmpCount + i);
-            }
+
+        int best = -1, localMin;
+        foreach (var coinDenomination in _coins.Where(c => c <= amount)) {
+          localMin = CoinChange(amount - coinDenomination);
+          if (best == -1 || (localMin >= 0 && localMin < best)) {
+            best = localMin;
           }
-          flag = flag || CoinChange(coins, amount, pos + 1, ref tmpCount);
-          if (flag) {
-            count = Math.Min(localMin, tmpCount);
-          }
-          return flag;
         }
+        return _map[amount] = best == -1 ? -1 : (best + 1);
       }
     }
 
