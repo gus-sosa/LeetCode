a4a108b8968b854159e4d43561a5c2b126aca1ee
Gus <g.sosacuervo1@gmail.com>
2020-03-13 00:02:48 -0500

    Solution to problem Factorial Trailing Zeros
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/113/math/816/

---
+++ InterviewQuestions/Math/FactorialTrailingZeros/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+
+namespace FactorialTrailingZeros {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      //for (int i = 10; i < 21; ++i) {
+      //  Console.WriteLine(s.TrailingZeroes(i));
+      //}
+      //Console.WriteLine(s.TrailingZeroes(20));
+      //Console.WriteLine(s.TrailingZeroes(21));
+      //Console.WriteLine();
+      //Console.WriteLine();
+      s.ComputeFactorials();
+      Console.WriteLine(s.TrailingZeroes(100));
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      public int TrailingZeroes(int n) {
+        if (n < 5) {
+          return 0;
+        }
+        if (n < 10) {
+          return 1;
+        }
+        n -= n % 5;
+        return n / 5 + TrailingZeroes(n / 5);
+      }
+
+      public void ComputeFactorials() {
+        BigInteger fact = 1;
+        for (int i = 1; i < 102; ++i) {
+          Console.WriteLine($"{i - 1}({NumTrailingZeros(fact)})={fact}");
+          fact *= i;
+        }
+      }
+
+      private int NumTrailingZeros(BigInteger fact) {
+        int numZeros = 0;
+        while (fact > 0) {
+          if (fact % 10 == 0) {
+            ++numZeros;
+            fact /= 10;
+          } else {
+            break;
+          }
+        }
+        return numZeros;
+      }
+    }
+
+
+    #endregion
+  }
+}
