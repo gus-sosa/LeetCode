93ef5b2923ea8077b3ec6ad6cbb6de970b53ec91
Gus <g.sosacuervo1@gmail.com>
2020-04-07 01:17:58 -0500

    Solution to problem Sum Of Two Integers
    https://leetcode.com/explore/interview/card/top-interview-questions-medium/114/others/822/

---
+++ InterviewQuestions/Math/SumOfTwoIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfTwoIntegers {
+  class Program {
+    static void Main(string[] args) {
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      public int GetSum(int a, int b) {
+        int carry = (a & b) << 1;
+        int xor = a ^ b;
+        return carry == 0 ? xor : GetSum(xor, carry);
+      }
+    }
+
+
+    #endregion
+  }
+}
