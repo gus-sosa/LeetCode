7cf5d6a4fc6d3b9d3599b926190a20205b0b8aff
Gus <g.sosacuervo1@gmail.com>
2020-04-09 19:32:43 -0500

    Solution to problem Container With Most Water
    https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/830/

---
+++ InterviewQuestions/Array/ContainerWithMostWater/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerWithMostWater {
+  class Program {
+    static void Main(string[] args) {
+    }
+  }
+
+  #region MyRegion
+
+
+  public class Solution {
+    public int MaxArea(int[] height) {
+      int i = 0, j = height.Length - 1, maxArea = 0;
+      while (i < j) {
+        maxArea = Math.Max(maxArea, Math.Min(height[i], height[j]) * (j - i));
+        if (height[i] <= height[j]) {
+          ++i;
+        } else {
+          --j;
+        }
+      }
+      return maxArea;
+    }
+  }
+
+
+  #endregion
+}
