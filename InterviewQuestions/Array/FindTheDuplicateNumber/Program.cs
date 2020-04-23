6cc33fafc999402d627345703a32848ef2c7231f
GusTaV <g.sosacuervo1@gmail.com>
2020-04-23 02:00:39 -0500

    Solution to problem Find The Duplicate Number
    https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/834/

---
+++ InterviewQuestions/Array/FindTheDuplicateNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FindTheDuplicateNumber {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+
+  #region MyRegion
+
+
+  public class Solution {
+    public int FindDuplicate(int[] nums) {
+      // Find the intersection point of the two runners.
+      int tortoise = nums[0];
+      int hare = nums[0];
+      do {
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+      } while (tortoise != hare);
+
+      // Find the "entrance" to the cycle.
+      tortoise = nums[0];
+      while (tortoise != hare) {
+        tortoise = nums[tortoise];
+        hare = nums[hare];
+      }
+
+      return hare;
+    }
+  }
+
+
+  #endregion
+}
