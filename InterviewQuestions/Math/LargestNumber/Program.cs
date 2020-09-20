c5c7883bb48ea7e630bf1f661c0d66e478ee2452
GusTaV <g.sosacuervo1@gmail.com>
2020-09-19 19:40:57 -0500

    Solution to problem Largest Number
    https://leetcode.com/problems/largest-number/submissions/

---
+++ InterviewQuestions/Math/LargestNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace LargestNumber {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      int[] input;
+
+      input = new int[] { 10, 2 };
+      Console.WriteLine(s.LargestNumber(input));
+      Console.WriteLine(s.LargestNumber(input) == "210");
+
+      input = new int[] { 3, 30, 34, 5, 9 };
+      Console.WriteLine(s.LargestNumber(input));
+      Console.WriteLine(s.LargestNumber(input) == "9534330");
+    }
+
+    #region MyRegion
+
+
+    public class Solution {
+      public string LargestNumber(int[] nums) {
+        string[] strNumbs = nums.Select(i => Convert.ToString(i)).ToArray();
+        Array.Sort(strNumbs, new NumSorter());
+        return strNumbs.Aggregate("", (acc, curr) => acc + curr);
+      }
+    }
+
+    internal class NumSorter : IComparer<string> {
+      public int Compare(string x, string y) {
+        return -(x + y).CompareTo(y + x);
+      }
+    }
+
+    #endregion
+  }
+
+
+}
