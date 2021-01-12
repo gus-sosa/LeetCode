6548fa321b2c168d814b45a3c188b1b8102ac02f
Gus <gcuervo@gtreasury.com>
2021-01-11 21:18:52 -0600

    First solution to the problem Largest Rectangle in Histogram
    https://leetcode.com/explore/interview/card/top-interview-questions-hard/124/others/877/

---
+++ InterviewQuestions/Others/LargestRectangleInHistogram/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LargestRectangleInHistogram
+{
+  class Program
+  {
+    static void Main(string[] args) {
+      var s = new Solution();
+      Console.WriteLine(s.LargestRectangleArea(new[] { 2, 1, 5, 6, 2, 3 }) == 10);
+      Console.WriteLine(s.LargestRectangleArea(new[] { 2, 4 }) == 4);
+      Console.WriteLine(s.LargestRectangleArea(new[] { 2 }) == 2);
+    }
+
+    #region MyRegion
+
+    public class Solution
+    {
+      public int LargestRectangleArea(int[] heights) {
+        var stack = new Stack<int>();
+        int maxArea = 0, indexTop = 0, areaWithCurrent = 0, i = 0;
+
+        for (; i < heights.Length; ++i) {
+          if (stack.Count == 0 || stack.Peek() <= heights[i]) {
+            stack.Push(i);
+          } else {
+            indexTop = stack.Pop();
+            areaWithCurrent = heights[indexTop] * (stack.Count == 0 ? i : i - stack.Peek() - 1);
+            if (maxArea < areaWithCurrent) {
+              maxArea = areaWithCurrent;
+            }
+          }
+        }
+
+        while (stack.Count > 0) {
+          indexTop = stack.Pop();
+          areaWithCurrent = heights[indexTop] * (stack.Count == 0 ? i : i - stack.Peek() - 1);
+          if (maxArea < areaWithCurrent) {
+            maxArea = areaWithCurrent;
+          }
+        }
+
+        return maxArea;
+      }
+    }
+
+    #endregion
+  }
+}
