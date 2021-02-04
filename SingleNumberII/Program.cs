d580400ef302ce6843a501c7233764d57214e8dd
GusTaV <g.sosacuervo1@gmail.com>
2021-02-04 10:18:48 -0600

    Solution to problem Single Number II
    https://leetcode.com/problems/single-number-iii/submissions/

---
+++ SingleNumberII/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SingleNumberII {
+  class Program {
+    static void Main(string[] args) {
+      var s = new Solution();
+      var res = s.SingleNumber(new int[] { 2, 1, 2, 3, 4, 1 });
+      Console.WriteLine(res[0]);
+      Console.WriteLine(res[1]);
+    }
+
+    #region MyRegion
+
+    public class Solution {
+      public int[] SingleNumber(int[] nums) {
+        int allXor = 0, lastBit = 0, firstPartXor = 0, secondPartXor = 0;
+        foreach (var item in nums) {
+          allXor ^= item;
+        }
+        lastBit = allXor & -allXor;
+        foreach (var item in nums) {
+          if ((item & lastBit) == 0) {
+            firstPartXor ^= item;
+          } else {
+            secondPartXor ^= item;
+          }
+        }
+        return new int[] { firstPartXor, secondPartXor };
+      }
+    }
+
+
+    #endregion
+  }
+}
