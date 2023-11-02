fc653042914f0125bbb3fed5a0018e194adea309
Gustavo Sosa Cuervo <g.sosacuervo1@gmail.com>
2023-11-01 23:48:15 -0500

    Making little optimization

--- RestoreIPAddresses/Solution.java
+++ RestoreIPAddresses/Solution.java
@@ -12,14 +12,20 @@ class Solution {
 
         var retVal = new ArrayList<String>();
         for (int i = 0; i < s.length(); i++) {
+            String segment1 = s.substring(0, i);
+            if (!areValidSegments(segment1)) {
+                continue;
+            }
             for (int j = i + 1; j < s.length(); j++) {
+                String segment2 = s.substring(i, j);
+                if (!areValidSegments(segment2)) {
+                    continue;
+                }
                 for (int k = j + 1; k < s.length(); k++) {
-                    String segment1 = s.substring(0, i);
-                    String segment2 = s.substring(i, j);
                     String segment3 = s.substring(j, k);
                     String segment4 = s.substring(k);
 
-                    if (isValidIp(segment1, segment2, segment3, segment4)) {
+                    if (areValidSegments(segment3, segment4)) {
                         String ip = String.format(
                                 "%s.%s.%s.%s",
                                 segment1,
@@ -35,9 +41,10 @@ class Solution {
         return retVal;
     }
 
-    private Boolean isValidIp(String... segments) {
+    private Boolean areValidSegments(String... segments) {
         return List.of(segments).stream()
-                .allMatch(i -> i != null && !i.isEmpty() && Integer.parseInt(i) < 256 && !hasLeadingZeros(i));
+                .allMatch(i -> i != null && !i.isEmpty() && Long.parseLong(i) < 2147483648l && Integer.parseInt(i) < 256
+                        && !hasLeadingZeros(i));
     }
 
     private Boolean hasLeadingZeros(String segment) {
