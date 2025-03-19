e4c37c83f6b3d7e4c7dc3122f57595ef49bab2c1
Gustavo Sosa Cuervo <g.sosacuervo1@gmail.com>
2025-03-19 00:08:18 -0500

    Solution to problem Single Number III
    https://leetcode.com/problems/single-number-iii/description/

---
+++ SingleNumberIII/SingleNumberIII.java
@@ -0,0 +1,7 @@
+package SingleNumberIII;
+
+public class SingleNumberIII {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ SingleNumberIII/Solution.java
@@ -0,0 +1,15 @@
+package SingleNumberIII;
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
